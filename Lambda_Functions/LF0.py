ba08915ac7a8fa4b17702436e4bb43d45e45595f
karanmankar95 <61260957+karanmankar95@users.noreply.github.com>
2021-03-05 23:37:21 -0500

    Add files via upload

---
+++ Lambda_Functions/LF0.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    client = boto3.client('lex-runtime')
+
+    response = client.post_text(
+        botName='DiningConciergeBot',
+        botAlias='dcbot',
+        userId= 'bot',
+        sessionAttributes={
+            },
+        requestAttributes={
+        },
+        inputText = event['messages'][0]['unstructured']['text']
+    )
+    print("Message from bot:" +response["message"])
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response["message"])
+    }
